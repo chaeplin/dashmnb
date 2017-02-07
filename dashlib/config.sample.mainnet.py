c4b2ed31134fbf6e3bc74d54f85ca25d1de8f305
chaeplin <chaeplin@gmail.com>
2017-02-08 06:38:34 +0900

    update config.py
    explain each option

--- dashlib/config.sample.mainnet.py
+++ dashlib/config.sample.mainnet.py
@@ -1,9 +1,8 @@
 # config.py
 
 # start of config
-# network
-MAINNET = True  # mainnet
 
+############  for main net #################
 
 # https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
 # purpose' / coin_type' / account' / change / address_index
@@ -52,15 +51,27 @@ SSH_LOCAL_PORT = '29998'
 # masternode_config
 masternode_conf_file = 'masternode.conf'
 
-# default address to send coins in hw wallet if reveiving_address in masternode.conf is blank
+# default address to send payout coins in hw wallet if reveiving_address in masternode.conf is blank.
 # this is not changing payment address of mn
+# command -m and -x will use
 default_receiving_address = ''
 
 #
-max_gab = 5     # number of keys used on mn config
+# how many mns do you have, to check address on hw wallet
+# if bip32 path last used address for mn is like 44'/5'/1'/0/10 : address_index is 10
+# use max_gab = 11 (address_index + 1)
+max_gab = 5     
 
-max_amounts = 50     # max amounts of each unspent tx
+# settings to control send payout coins
+# when if MOVE_1K_COLLATERAL = True will not used to check
+
+# if a unspent tx(or payout or deposit on mn address) has amount more han this, will not touch
+max_amounts = 50
+
+# number of inputs to include in a tx
 max_unspent = 20
+
+# if number of payout is less than this, will not touch
 min_unspent = 1
 
 # if config.py and masternode.x.conf unchanged, recheck config every 7
@@ -70,19 +81,19 @@ config_cache_refresh_interval_hour = 7 * 24
 # txs
 txs_cache_refresh_interval_hour = 6
 
-#
-errorsnprogress = []
-
 # caution this config move 1K collateral to configured address with fee 0.0001
 MOVE_1K_COLLATERAL = False
 
-
 # don't change
 # dash mainnet
+# network
+MAINNET = True  # mainnet
 wif_prefix = 204  # cc
 addr_prefix = 76   # 4c
 coin_name = 'Dash'
 min_fee = 10000  # fee for tx
+# to display err on masternode.conf
+errorsnprogress = []
 # don't change
 
 # end of config.py
--- dashlib/config.sample.testnet.py
+++ dashlib/config.sample.testnet.py
@@ -1,8 +1,8 @@
 # config.py
 
 # start of config
-# network
-MAINNET = False  # testnet
+
+############  for test net #################
 
 # https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
 # purpose' / coin_type' / account' / change / address_index
@@ -52,17 +52,30 @@ SSH_LOCAL_PORT = '39998'
 # masternode_config
 masternode_conf_file = 'masternode.conf'
 
-# default address to send coins in hw wallet if reveiving_address in masternode.conf is blank
+# default address to send payout coins in hw wallet if reveiving_address in masternode.conf is blank.
 # this is not changing payment address of mn
+# command -m and -x will use
 default_receiving_address = ''
 
 #
-max_gab = 5     # number of keys used on mn config
+# how many mns do you have, to check address on hw wallet
+# if bip32 path last used address for mn is like 44'/5'/1'/0/10 : address_index is 10
+# use max_gab = 11 (address_index + 1)
+max_gab = 5     
 
-max_amounts = 50     # max amounts of each unspent tx
+# settings to control send payout coins
+# when if MOVE_1K_COLLATERAL = True will not used to check
+
+# if a unspent tx(or payout or deposit on mn address) has amount more han this, will not touch
+max_amounts = 50
+
+# number of inputs to include in a tx
 max_unspent = 20
+
+# if number of payout is less than this, will not touch
 min_unspent = 1
 
+# setting to control parsed cache(config, masternode.conf, payout txs)
 # if config.py and masternode.x.conf unchanged, recheck config every 7
 # day, and 6 hour
 config_cache_refresh_interval_hour = 7 * 24
@@ -70,18 +83,20 @@ config_cache_refresh_interval_hour = 7 * 24
 # txs
 txs_cache_refresh_interval_hour = 6
 
-#
-errorsnprogress = []
-
 # caution this config move 1K collateral to configured address with fee 0.0001
 MOVE_1K_COLLATERAL = False
 
 
 # don't change
 # dash testnet
+# network
+MAINNET = False  # testnet
 wif_prefix = 239  # ef
 addr_prefix = 140  # 8c
 coin_name = 'tDash'
 min_fee = 10000  # fee for tx
-# don't change  
+# to display err on masternode.conf
+errorsnprogress = []
+# don't change 
+
 # end of config.py

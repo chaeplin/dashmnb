13e7874b31f81d88a8e9e94d2fbc94c0adabcea7
chaeplin <chaeplin@gmail.com>
2017-02-23 14:05:49 +0900

    add delay on vote

--- dashlib/mnb_vote.py
+++ dashlib/mnb_vote.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 
 import re
+import time
 from mnb_makevote import *
 from mnb_misc import *
 from mnb_rpc import *
@@ -31,5 +32,6 @@ def start_votes(
     for voteconf in listforvotes:
         r = rpc_voteraw(voteconf, access)
         print('%s - %s' % (voteconf['alias'], r))
+        time.sleep(1)
 
 
